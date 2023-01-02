5b079067c1f19058bddfc06a736854af80560b4f
jahidul96 <jahidul59895@gmail.com>
2023-01-02 11:38:42 +0600

    list code example!!

--- list_demo.dart
+++ list_demo.dart
@@ -2,29 +2,29 @@ void main() {
   List list = [1, 2, 3, 4, 5, 6, 7];
 
 // adding one or multiple
-  // list.add(8);
-  // list.addAll([9, 10, 11, 12]);
-  // print(list);
+  list.add(8);
+  list.addAll([9, 10, 11, 12]);
+  print(list);
 
   // container check
-  // print(list.contains(1));
+  print(list.contains(1));
 
 // remove item at specific index
-  // list.removeAt(4);
+  list.removeAt(4);
 
   // reduce method!!
-  // print(list.reduce((value, element) => value + element));
-  // print(list);
+  print(list.reduce((value, element) => value + element));
+  print(list);
 
   // for loop
-  // for (var i = 0; i < list.length; i++) {
-  //   print("item in list " + list[i].toString());
-  // }
+  for (var i = 0; i < list.length; i++) {
+    print("item in list " + list[i].toString());
+  }
 
 // for in loop in a list
-  // for (var element in list) {
-  //   print("item in list $element");
-  // }
+  for (var element in list) {
+    print("item in list $element");
+  }
 
   print("list element are $list");
 
