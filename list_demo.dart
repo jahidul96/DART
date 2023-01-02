647995c18a0205890cc32f9a3cdf44fba813255e
jahidul96 <jahidul59895@gmail.com>
2023-01-02 11:36:34 +0600

    list code added!!

--- list_demo.dart
+++ list_demo.dart
@@ -1,29 +1,35 @@
 void main() {
   List list = [1, 2, 3, 4, 5, 6, 7];
 
-  // list.add(8);
-  // list.addAll([9, 10, 11, 12]);
-  // print(list.contains(1));
-  // print(list);
+// adding one or multiple
+  list.add(8);
+  list.addAll([9, 10, 11, 12]);
+  print(list);
 
-  // list.removeAt(4);
+  // container check
+  print(list.contains(1));
+
+// remove item at specific index
+  list.removeAt(4);
+
+  // reduce method!!
   print(list.reduce((value, element) => value + element));
   print(list);
 
   // for loop
-
-  // for (var i = 0; i < list.length; i++) {
-  //   print("item in list " + list[i].toString());
-  // }
+  for (var i = 0; i < list.length; i++) {
+    print("item in list " + list[i].toString());
+  }
 
 // for in loop in a list
-  // for (var element in list) {
-  //   print("item in list $element");
-  // }
+  for (var element in list) {
+    print("item in list $element");
+  }
 
-  // print("list element are $list");
+  print("list element are $list");
 
-  // list.clear();
+// clear all
+  list.clear();
 
-  // print(list);
+  print(list);
 }
